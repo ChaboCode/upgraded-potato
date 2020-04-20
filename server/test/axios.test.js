c9bc7628be6c7a580169b2753d1e33155cadcf2b
TheBesterCode <thebestergamergd@gmail.com>
2020-04-19 20:44:10 -0500

    tests tha i forgot to commit lol

--- server/test/axios.test.js
+++ server/test/axios.test.js
@@ -1,12 +1,13 @@
 const assert = require('chai').assert,
-      axios = require('axios'),
       mongoose = require('mongoose'),
       db = require('../database/db').db,
-      Teachers = require('../models/TeacherSchema.js'),
-      sha256 = require('js-sha256').sha256
+      Teachers = require('../models/TeacherSchema'),
+      sha256 = require('js-sha256').sha256,
+      nock = require('nock'),
+      getTotalStudents = require('./axios.js').getTotalStudents
 
 describe('some shitty test to check a stupid schema', () => {
-    before(async () => {
+    beforeEach(async () => {
         await mongoose.connect(db, {
             useNewUrlParser: true,
             useCreateIndex: true
@@ -17,13 +18,32 @@ describe('some shitty test to check a stupid schema', () => {
             }
         })
     })
-    it('should return Saul', (done) => {
+    test('should return Saul', (done) => {
         Teachers.findOne({
             key: sha256(sha256('juan01'))
         }, (err, data) => {
             if (err) return err
-            assert(data.name == 'Saul')
+            assert.equal(data.name, 'Saul')
             done()
+        })
+    })
+
+    describe('Update functions', () => {
+        test('Should add NEW_REG to TEST.groups.TEST_GROUP.regs.TEST_ACTS', (done) => {
+            Teachers.findOne({
+                key: sha256(sha256(''))
+            }, async (err, data) => {
+                if (err) return err
+                console.log(data.groups.TEST_GROUP.regs)
+                data.groups.TEST_GROUP.regs.TEST_ACTS.NEW_REG = 'NEW_REG'
+                data.markModified('groups')
+                await data.save((err, new_data) => {
+                    if (err) return err
+                    assert.equal(new_data.groups.TEST_GROUP.regs.TEST_ACTS.NEW_REG, 'NEW_REG')
+                    console.log(new_data.groups.TEST_GROUP.regs)
+                    done()
+                })
+            })
         })
     })
 })
