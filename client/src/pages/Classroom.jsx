9a8eeaed4ab60ea8f773dd1c5f9564298962eaaa
TheBesterCode <thebestergamergd@gmail.com>
2020-04-21 10:14:24 -0500

    Re-indented

--- client/src/pages/Classroom.jsx
+++ client/src/pages/Classroom.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
 
 import './css/Classroom.css'
@@ -7,78 +7,82 @@ import Table from '../components/Table'
 
 class Classroom extends Component {
 
-    constructor(props) {
-        super(props)
-        this.groups = this.props.groups
-        this.key = this.props._key
+  constructor(props) {
+    super(props)
+    this.groups = this.props.groups
+    this.key = this.props._key
 
-        this.state = {
-            group: '',
-            students: [],
-            sup_regs: [],
-            update: false
-        }
-        this.handleGroupClick = this.handleGroupClick.bind(this)
-        this.updateGroup = this.updateGroup.bind(this)
+    this.state = {
+      group: '',
+      students: [],
+      sup_regs: [],
+      update: false,
+      table: false
     }
+    this.handleGroupClick = this.handleGroupClick.bind(this)
+    this.updateGroup = this.updateGroup.bind(this)
+  }
 
-    async handleGroupClick(e, group) {
-        e.preventDefault()
-        const students = await axios.post('http://localhost:5000/group/getStudentsByName', {name: group})
-        
-        const sup_regs = await axios.post('http://localhost:5000/teacher/getGroupRegisters', {key: this.key, group: group})
-        this.setState({
-            sup_regs: sup_regs.data.Actividades, //TODO
-            students: students.data,
-            group: group
-        })
+  async handleGroupClick(e, group) {
+    e.preventDefault()
+    const students = await axios.post('http://localhost:5000/group/getStudentsByName', {name: group})
+
+    const sup_regs = await axios.post('http://localhost:5000/teacher/getGroupRegisters', {key: this.key, group: group})
+    this.setState({
+      sup_regs: sup_regs.data.Actividades, //TODO
+      students: students.data,
+      group: group,
+      table: true
+    })
+  }
+
+  async updateGroup(group) {
+    const students = await axios.post('http://localhost:5000/group/getStudentsByName', {name: group})
+
+    const sup_regs = await axios.post('http://localhost:5000/teacher/getGroupRegisters', {key: this.key, group: group})
+    this.setState({
+      sup_regs: sup_regs.data.Actividades, //TODO
+      students: students.data,
+      group: group
+    })
+  }
+
+  render() {
+    let groups = this.groups.map(group => {
+      return (
+          <li className="group"
+              onClick={(e) => this.handleGroupClick(e, group)}><span>{group}</span></li>
+      )
+    })
+    const data = {
+      group: this.state.group,
+      teacher: this.key,
+      group_length: this.state.students.length,
+      update: this.updateGroup
     }
+    return (
+        <>
+          <header id="top-bar">
+            <span id="tech">Kaerdos</span> School Tool
+          </header>
 
-    async updateGroup(group) {
-        const students = await axios.post('http://localhost:5000/group/getStudentsByName', {name: group})
-        
-        const sup_regs = await axios.post('http://localhost:5000/teacher/getGroupRegisters', {key: this.key, group: group})
-        this.setState({
-            sup_regs: sup_regs.data.Actividades, //TODO
-            students: students.data,
-            group: group
-        })
-    }
+          <aside id="groups">
+            <div className="sign">Grupos</div>
+            {groups}
+          </aside>
 
-    render() {
-        let groups = this.groups.map(group => {
-            return (
-            <li className="group" 
-                onClick={(e) => this.handleGroupClick(e, group)}><span>{group}</span></li>
-            )
-        })
-        const data = {
-            group: this.state.group,
-            teacher: this.key,
-            group_length: this.state.students.length,
-            update: this.updateGroup
-        }
-        return (
-            <Fragment>
-                <header id="top-bar">
-                    <span id="tech">Kaerdos</span> School Tool
-                </header>
-
-                <aside id="groups">
-                    <div className="sign">Grupos</div>
-                    {groups}
-                </aside>
-
-                <nav id="tabs">
-                    <span className="tab">Asistencias</span>
-                    <span className="tab">Trabajos</span>
-                </nav>
-                        <div id="table">
-                            <Table sup_regs={this.state.sup_regs} students={this.state.students} data={data} />
-                        </div>
-            </Fragment>
-        )
-    }
+          <nav id="tabs">
+            <span className="tab">Asistencias</span>
+            <span className="tab">Trabajos</span>
+          </nav>
+          {this.state.table ? (
+              <div id="table">
+                <Table sup_regs={this.state.sup_regs} students={this.state.students} data={data} />
+              </div>
+          ) : null}
+        </>
+    )
+  }
 }
 
 export default Classroom
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,59 +6,59 @@ import './css/Home.css'
 import Classroom from './Classroom'
 
 class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            logged: false,
-            key: ''
-        }
-        this.handleChange = this.handleChange.bind(this)
+  constructor(props) {
+    super(props)
+    this.state = {
+      logged: false,
+      key: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  async handleClick(e) {
+    e.preventDefault()
+    const response = await axios.post('http://localhost:5000/teacher/getGroups', {key: this.state.key})
+    const groups = response.data
+    let names = []
+    for (let key in groups) {
+      names.push(key)
     }
 
-    async handleClick(e) {
-        e.preventDefault()
-        const response = await axios.post('http://localhost:5000/teacher/getGroups', {key: this.state.key})
-        const groups = response.data
-        let names = []
-        for (let key in groups) {
-            names.push(key)
-        }
+    this.setState({
+      logged: true,
+      data: {
+        groups: names
+      }
+    })
+  }
 
-        this.setState({
-            logged: true,
-            data: {
-                groups: names
-            }
-        })
-    }
+  handleChange(e) {
+    e.preventDefault()
+    this.setState({
+      logged: this.state.logged,
+      key: sha256(e.target.value)
+    })
+  }
 
-    handleChange(e) {
-        e.preventDefault()
-        this.setState({
-            logged: this.state.logged,
-            key: sha256(e.target.value)
-        })
-    }
-
-    render() {
-        return this.state.logged ?
-            (
-                <Classroom groups={this.state.data.groups} _key={this.state.key}/>
-            )
-            : (
-                <div id="main-container">
-                    <div id="top-bar">
-                        Kaerdos
-                    </div>
-                    <div id="container">
-                        <span id="hello">Bienvenido</span> <br/>
-                        <span id="message">Ingrese su clave</span> <br/>
-                        <input onChange={this.handleChange} type="password" placeholder="Clave de profesor"/>
-                        <button onClick={e => this.handleClick(e)}>Entrar</button>
-                    </div>
-                </div>
-            )
-    }
+  render() {
+    return this.state.logged ?
+        (
+            <Classroom groups={this.state.data.groups} _key={this.state.key}/>
+        )
+        : (
+            <div id="main-container">
+              <div id="top-bar">
+                Kaerdos
+              </div>
+              <div id="container">
+                <span id="hello">Bienvenido</span> <br/>
+                <span id="message">Ingrese su clave</span> <br/>
+                <input onChange={this.handleChange} type="password" placeholder="Clave de profesor"/>
+                <button onClick={e => this.handleClick(e)}>Entrar</button>
+              </div>
+            </div>
+        )
+  }
 }
 
 export default Home
